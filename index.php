1af9a6c456d436018a4f490a6965dd6ef4da7755
Bi1221 <bianca_bartole@hotmail.com>
2024-05-09 16:00:40 -0300

    projeto agenda

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Nome do sistema</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <div class="login">
+        <div class="form">
+            <form class="registro">
+                <input type="email" name="usuario">
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
