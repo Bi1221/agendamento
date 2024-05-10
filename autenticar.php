ac2b702665368b4b2df86f6072846191de31eb8b
Bianca <biancabartole23@gmail.com>
2024-05-10 19:58:38 -0300

    aula26

--- autenticar.php
+++ autenticar.php
@@ -1,10 +1,17 @@
 <?php
-require_once ("conexao.php");
+require_once("conexao.php");
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
 
-$query = $pdo->query("SELECT * from usuarios where email = '$usuario'and senha = '$senha'");
+$query = $pdo->prepare("SELECT * from usuarios where email = :email and senha = :senha");
+$query->bindValue(":email", "$usuario");       //Aula26
+$query->bindValue(":senha", "$senha");         //Aula26
+$query->execute();
+
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = count($res);
-echo $linhas;
-?>
+
+if($linhas > 0){                 //Aula26
+    echo 'Login permitido';      //Aula26
+
+}
