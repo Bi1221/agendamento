779cbc36affc7fc2b48f101f1aa2df3ffa507d54
Bi1221 <bianca_bartole@hotmail.com>
2024-05-10 16:45:06 -0300

    conexão com banco de dados

---
+++ conexao.php
@@ -0,0 +1,19 @@
+<?php 
+//definir fuso horário
+date_default_timezone_set('America/Sao_Paulo');
+
+//dados banco de dados local
+$servidor = 'localhost';
+$banco = 'agendamento';
+$usuario = 'root';
+$senha = '';
+
+try {
+    $pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+} catch (Exception $e) {
+    echo 'Erro ao envontrar ao banco de dados!<br>';
+    echo $e;
+    
+
+}
+?>
--- index.php
+++ index.php
@@ -4,13 +4,15 @@
 <head>
     <title>Nome do sistema</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="img/icone.png">
 </head>
 
 <body>
     <div class="login">        
         <div class="form">
-        <img src="img/logo.png">
-            <form class="registro">
+        <img src="img/logo.png" class="imagem">
+            <form method="post" action="autenticar.php">
                 <input type="email" name="usuario" placeholder="Seu email">
                 <input type="password" name="senha"placeholder="Sua senha">
                 <button>Login</button>
