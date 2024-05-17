eaa63aac5cfc84af3c651ded6be3ccd6ca5e2154
Bi1221 <bianca_bartole@hotmail.com>
2024-05-17 16:11:14 -0300

    aula5

--- painel/paginas/usuarios.php
+++ painel/paginas/usuarios.php
@@ -10,3 +10,87 @@ $pag = 'usuarios';
 
 <script type="text/javascript">var pag = "<?=$pag?>"</script>
 <script src="js/ajax.js"></script>
+
+
+<div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="exampleModalLabel"><span id="titulo_inserir"></span>/h4>
+				<button id="btn-fechar" type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-top: -25px">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form id="form">
+			<div class="modal-body">
+				
+
+					<div class="row">
+						<div class="col-md-6">							
+								<label>Nome</label>
+								<input type="text" class="form-control" id="nome" name="nome" placeholder="Seu Nome" required>							
+						</div>
+
+						<div class="col-md-6">							
+								<label>Email</label>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Seu Nome" required>							
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="col-md-4">							
+								<label>Telefone</label>
+								<input type="text" class="form-control" id="telefone" name="telefone" placeholder="Seu Telefone"  required>							
+						</div>
+
+                        <div class="col-md-4">							
+								<label>Nível</label>
+                                <select class="form-control" name="nivel" id="nivel"> 
+                                    <option>Administrador</option>
+                                    <option>Comum</option>
+                                </select>
+						</div>
+
+						
+
+						
+					</div>
+
+					<div class="row">
+						<div class="col-md-12">		
+						<label>Endereço</label>
+						<input type="text" class="form-control" id="endereco_perfil" name="endereco" placeholder="Seu Endereço" value="<?php echo $endereco_usuario ?>" >							
+
+					</div>
+
+
+
+					<div class="row">
+						<div class="col-md-8">							
+								<label>Foto</label>
+								<input type="file" class="form-control" id="foto_perfil" name="foto" value="<?php echo $foto_usuario ?>" onchange="carregarImgPerfil()">							
+						</div>
+
+						<div class="col-md-4">								
+							<img src="images/perfil/<?php echo $foto_usuario ?>"  width="80px" id="target-usu">								
+							
+						</div>
+
+						
+					</div>
+
+
+					<input type="hidden" name="id_usuario" value="<?php echo $id_usuario ?>">
+				
+
+				<br>
+				<small><div id="msg-perfil" align="center"></div></small>
+			</div>
+			<div class="modal-footer">       
+				<button type="submit" class="btn btn-primary">Salvar</button>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
