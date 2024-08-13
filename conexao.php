a070a9109d0cd53134d213fe1e76e4175c1c60b7
Marina Angela Nobre Araújo Maciel <marinaangela864@gmail.com>
2024-08-13 08:54:13 -0300

    Sua mensagem de commit

---
+++ conexao.php
@@ -0,0 +1,63 @@
+<?php 
+
+//definir fuso horário
+date_default_timezone_set('America/Sao_Paulo');
+
+$url_sistema = "http://$_SERVER[HTTP_HOST]/";
+$url = explode("//", $url_sistema);
+if($url[1] == 'localhost/'){
+	$url_sistema = "http://$_SERVER[HTTP_HOST]/agendamento/";
+}
+
+//dados conexão bd local
+$servidor = 'localhost';
+$banco = 'agendamento';
+$usuario = 'root';
+$senha = '';
+
+try {
+	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+} catch (Exception $e) {
+	echo 'Erro ao conectar ao banco de dados!<br>';
+	echo $e;
+}
+
+
+//variaveis globais
+$nome_sistema = 'Nome Sistema';
+$email_sistema = 'drajessicainoue@gmail.com';
+$telefone_sistema = '(11) 98521-4534';
+
+$query = $pdo->query("SELECT * from config");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+if($linhas == 0){
+	$pdo->query("INSERT INTO config SET nome = '$nome_sistema', email = '$email_sistema', telefone = '$telefone_sistema', logo = 'logo.png', logo_rel = 'logo.jpg', icone = 'icone.png', ativo = 'Sim'");
+}else{
+$nome_sistema = $res[0]['nome'];
+$email_sistema = $res[0]['email'];
+$telefone_sistema = $res[0]['telefone'];
+$endereco_sistema = $res[0]['endereco'];
+$logo_sistema = $res[0]['logo'];
+$logo_rel = $res[0]['logo_rel'];
+$icone_sistema = $res[0]['icone'];
+$ativo_sistema = $res[0]['ativo'];
+
+if($ativo_sistema != 'Sim' and $ativo_sistema != ''){ ?>
+	<style type="text/css">
+		@media only screen and (max-width: 700px) {
+ .imgsistema_mobile{
+    width:300px;
+  }
+    
+}
+	</style>
+	<div style="text-align: center; margin-top: 100px">
+	<img src="img/bloqueio.png" class="imgsistema_mobile">	
+	</div>
+<?php 
+exit();
+} 
+
+}	
+ ?>
